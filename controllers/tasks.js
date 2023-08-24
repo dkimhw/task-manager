6d500ccc52a6bba861dccd626e64a0c9641e7032
David Kim <dkhw90@gmail.com>
2023-08-24 19:52:31 +0200

    Add controller logic for delete & get task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,23 +3,66 @@ import Task from '../models/task.js';
 
 
 
-export const getAllTasks = (req, res) => {
-  res.send('all tasks')
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json({'msg': err});
+  }
 }
 
 export const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json(task);
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).json({'msg': err});
+  }
 }
 
-export const getTask = (req, res) => {
-  res.send({...req.params, 'type': 'get' });
+export const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({
+      _id: taskID
+    });
+
+    if (!task) {
+      return res.status(404).json({
+        msg: `No task with id: ${taskID}`
+      });
+    }
+
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({'msg': err});
+  }
 }
 
-export const updateTask = (req, res) => {
-  res.send({...req.params, 'type': 'update' });
+export const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const {
+      completed,
+      task
+    } = req.body;
+
+
+    res.status(200).json({id: taskID, data: req.body});
+  } catch (err) {
+
+  }
 }
 
-export const deleteTask = (req, res) => {
-  res.json({...req.params, 'type': 'delete' });
+export const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) return res.status(404).json({'msg': `No task id with ${taskID}`});
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({'msg': err});
+  }
 }
